fa43b678a639fbe05974252c83ec50d76fdd52b0
Saul <saulydominguez@gmail.com>
2017-10-13 19:26:10 +0200

    Slightly upgraded to find almost every crushes

--- thiscrush.py
+++ thiscrush.py
@@ -7,6 +7,8 @@ num_pages = input('Number of pages to get: ')
 crushes_num = 0
 anon_num = 0
 
+out_file = open('{}.txt'.format(user),'a')
+
 for num_page in range(int(num_pages)):
     page = requests.get('http://www.thiscrush.com/~{}/{}'.format(str(user),str(num_page+1))).content
 
@@ -18,11 +20,15 @@ for num_page in range(int(num_pages)):
         for box in boxes:
             try:
                 msg = box.find("p").text.strip().replace('\t','').replace('\r\n','').split('\n')
+                try:
+                    msg.remove('')
+                except ValueError:
+                    pass
 
                 # Obtaining message content, message author and message date
-                msg_content = msg[0]
-                msg_author = msg[1]
-                msg_date = msg[2][13:]
+                msg_content = msg[0].encode('utf-8')
+                msg_author = msg[1][1:].encode('utf-8')
+                msg_date = box.find('span').text[13:]
 
                 # Obtaining month, day, time and year
                 date_broken = msg_date.split()
@@ -30,19 +36,23 @@ for num_page in range(int(num_pages)):
                 day = date_broken[1][:-1]
                 year = date_broken[2]
                 time = date_broken[3]
-                print(month,day,year,time)
 
-                print(msg_content,msg_author,sep='\n',end='\n\n')
+                out_file.write('{} - {} - {} | {}\n'.format(day,month,year,time))
+                out_file.write('{}\n'.format(msg_content))
+                out_file.write('{}\n'.format(msg_author))
+                out_file.write('\n')
 
-                if msg_author == '-Anonymous':
+                if msg_author == b'Anonymous':
                     anon_num += 1
                 crushes_num += 1
-            except AttributeError:
+            except AttributeError as e:
                 pass
-            except IndexError:
-                pass
+            except IndexError as e:
+                 pass
     else:
         break
 
 print('Got',crushes_num,'crushes')
 print('{} ({:.2%}) crushes are anonymous'.format(anon_num,anon_num/crushes_num))
+
+out_file.close()

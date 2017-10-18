feaf873925c542d3d27ac88d54ce2db639631f41
Saul <saulydominguez@gmail.com>
2017-10-18 11:30:59 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='thiscrush-scraping',
+      version='1.0',
+      description='A thiscrush scraper',
+      author='Saul Dominguez',
+      author_email='saulydominguez@gmail.com',
+      url='https://github.com/sauldom102/thiscrush-scraping',
+      classifiers=[],
+      packages=['thiscrush'],
+      install_requires=['requests','bs4'],
+      )
